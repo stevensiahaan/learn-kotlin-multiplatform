1305184e4bb2ed7ce5d12de350369b2fe94ff47a
stevensiahaan <stevenjhsiahaan@gmail.com>
2019-09-11 10:57:58 +0700

    initialize project :tada: :tada: :tada:

---
+++ app/src/main/java/id/forbeestudio/learnkotlinmultiplatform/MainActivity.kt
@@ -0,0 +1,16 @@
+package id.forbeestudio.learnkotlinmultiplatform
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import createApplicationScreenMessage
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        main_text.text = createApplicationScreenMessage()
+    }
+}
